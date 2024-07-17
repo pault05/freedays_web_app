a6f4e27451fe59f1f38f25008905f8ec5b9de926
Adrian <oprisadi225@gmail.com>
2024-07-17 15:13:32 +0300

    working bootstrap, finished admin view holiday, adaugat model admin view (era lipsa)

---
+++ app/Models/AdminView.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class AdminView extends Model
+{
+    use HasFactory;
+}

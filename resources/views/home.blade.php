755fcfd981632f59fcebc1f0959a141d37749ba8
pault05 <147524535+pault05@users.noreply.github.com>
2024-07-17 16:00:26 +0300

    Home update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,30 +1,40 @@
 @extends('Components.layout')
 
-<title>Home Page</title>
-
-
 @section('content')
-    <div>
-        <form action="/user-profile" method="GET">
-            <input type="submit" value="User Profile" />
-        </form>
 
+    <div class="card p-3 shadow-sm mb-5">
+        <h1>Home Page</h1>
+    </div>
+
+    <div class="mb-3">
+        <form action="/user-profile" method="GET">
+            <input type="submit" value="User Profile"  />
+        </form>
+    </div>
+
+    <div class="mb-3">
         <form action="/free-day-request" method="GET">
             <input type="submit" value="Free Day Request" />
         </form>
+    </div>
 
+    <div class="mb-3">
         <form action="/account-creation" method="GET">
             <input type="submit" value="Create a New Account" />
         </form>
+    </div>
 
+    <div class="mb-3">
         <form action="/admin-view" method="get">
             <input type="submit" value="Admin Dashboard" />
         </form>
+    </div>
 
+    <div class="mb-3">
         <form action="/login" method="POST">
             <input type="submit" value="Login" />
         </form>
-
     </div>
+
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,15 @@
 <?php
 
-use App\Http\Controllers\FreeDayRequestController;
+use App\Http\Controllers\FreeDaysRequestController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
 
 
-Route::get('/free-day-request', [FreeDayRequestController::class, 'index']);
+Route::get('/free-day-request', [FreeDaysRequestController::class, 'index']);
 Route::get('/account-creation', [\App\Http\Controllers\AccountCreationController::class, 'index']);
 Route::get('/admin-view', [\App\Http\Controllers\AdminViewController::class, 'index']);
 Route::post('/login', [\App\Http\Controllers\LoginController::class, 'index']);
 Route::get('/user-profile', [\App\Http\Controllers\UserProfileController::class, 'index']);
-
-
-
-
-
-

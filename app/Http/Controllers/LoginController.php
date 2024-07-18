87a256612fa39d562090a3cfaa6129b3233730c8
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-07-18 13:44:02 +0300

    login form update 1.0

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,13 +3,35 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Validation\ValidationException;
+use function PHPUnit\Framework\throwException;
 
 class LoginController extends Controller
 {
     // login tudor
-    public function index(Request $request)
+    public function create()
     {
         return view('login');
+
+    }
+
+    public function store()
+    {
+        $attributes = request()->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+       if(! Auth::attempt($attributes)){
+           throw ValidationException::withMessages([
+               'email' => ['The provided credentials are incorrect.']
+           ]);
+       }
+
+        request()->session()->regenerate();
+
+        return redirect('/home');
 
     }
 
--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,19 @@
 <?php
 
-use App\Http\Controllers\FreeDayRequestController;
+use App\Http\Controllers\FreeDaysRequestController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminViewController;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/home', function () {
+    return view('home');
 });
 
 
 Route::get('/free-day-request', [\App\Http\Controllers\FreeDaysRequestController::class, 'index']);
 Route::get('/account-creation', [\App\Http\Controllers\AccountCreationController::class, 'index']);
 Route::get('/admin-view', [\App\Http\Controllers\AdminViewController::class, 'index']);
-Route::post('/login', [\App\Http\Controllers\LoginController::class, 'index']);
+Route::get('/login', [\App\Http\Controllers\LoginController::class, 'create'])->name('login');
+Route::post('/login', [\App\Http\Controllers\LoginController::class, 'store']);
 Route::get('/user-profile', [\App\Http\Controllers\UserProfileController::class, 'index']);
 
 Route::get('/official-holiday', [\App\Http\Controllers\OfficialHolidayController::class, 'index']);

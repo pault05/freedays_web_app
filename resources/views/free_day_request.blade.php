dc1f66346b2d3d9749c2d71989f9e4dd62e10e2d
Georgiana Stoie <137824525+Georgiana1254@users.noreply.github.com>
2024-07-23 10:37:02 +0300

    update bd + frontend

--- resources/views/free_day_request.blade.php
+++ resources/views/free_day_request.blade.php
@@ -31,33 +31,29 @@
         <div class="container mt-3">
             <form id="leave-form" action="/free-day-request/save" method="POST">
                 <div class="row">
-                    <div class="col-sm-1 col-md-2 col-lg-3 mb-3">
+                    <div class="col-12 col-sm-6 col-md-4 mb-3">
                         <label for="days-left">Days off left</label>
                         <p class="ms-1">{{ 21 - $request_leave['approved'] }}</p>
                     </div>
                 </div>
-                <div class="row">
+                <div class="row d-flex justify-content-start me-3">
                     @csrf
-                    <div class="col-sm-1 col-md-2 col-lg-3 mb-2">
+                    <div class="col-lg-3 col-sm-12 col-md-6 mb-2">
                         <label for="start-date">Start date</label>
-                        <div class="row w-75">
                             <input type="date" class="form-control" id="start-date" name="start-date">
-                        </div>
                     </div>
-                    <div class="col-sm-1 col-md-2 col-lg-3 mb-2">
+                    <div class="col-sm-12 col-md-6 col-lg-3 mb-2">
                         <label for="end-date">End Date</label>
-                        <div class="row w-75">
                             <input type="date" class="form-control" id="end-date" name="end-date">
-                        </div>
                     </div>
-                    <div class="col-sm-1 col-md-2 col-lg-3 mb-2">
+                    <div class="col-sm-12 col-md-6 col-lg-3 mb-2">
                         <label for="days-left">Selected leave days</label>
                         <input type="text" class="form-control"
                                value="{{ session('days_left', '0') }}" id="days-left" placeholder="Selected leave days" readonly>
                         <input type="hidden" class="form-control" name="days" id="days"
                                value="{{ session('days_left', '0') }}" placeholder="Selected leave days">
                     </div>
-                    <div class="col-sm-1 col-md-2 col-lg-3 form-check mt-1 mb-2 ">
+                    <div class="col-sm-12 col-md-6 col-lg-3 form-check mt-1 mb-2 ">
                         <br>
                         <label class="form-check-label" for="half-day">Half day</label>
                         <input type="checkbox" class="form-check-input" name="half-day">
@@ -68,7 +64,7 @@
                     <div class="mb-3 col mb-2">
                         <label for="category" class="col-sm-2 col-form-label">Category</label>
                         <div class="col-sm-10">
-                            <select class="form-select" name="category_id" style="width: 50vw">
+                            <select class="form-select" name="category_id" style="width: 110%">
                                 <option selected>Choose category...</option>
                                 <option value="1">Paid Leave</option>
                                 <option value="2">Unpaid Leave</option>
@@ -82,24 +78,24 @@
                 <div class="row">
                     <div class="mb-2">
                         <label for="description" class="form-label">Description</label>
-                        <textarea class="form-control" name="description" rows="3" placeholder="Description" style="width:50vw"></textarea>
+                        <textarea class="form-control" name="description" rows="3" placeholder="Description" style="width:92%"></textarea>
                     </div>
                 </div>
 
                 <div class="row mt-3">
                     <div class="mt-3 row">
-                        <div class="col-sm-4">
-                            <input type="file" class="form-control mb-1" name="file-upload">
+                        <div class="col-sm-6" style="width: 95%">
+                            <input type="file" class="form-control mb-1" style="width: 100%" name="file-upload">
                         </div>
                     </div>
                 </div>
+                <div class="row ms-1 mt-5">
+                    <div class="d-flex justify-content-end" style="margin-left: 89%; width: 1%">
+                        <a href="/home" type="button" class="btn btn-primary">Back</a>
+                        <button type="submit" class="btn btn-primary ms-3" form="leave-form" id="submit">Submit</button>
+                    </div>
+                </div>
             </form>
-        </div>
-    </div>
-    <div class="row ms-1 mt-5">
-        <div class="d-flex justify-content-end" style="margin-left: 0">
-            <a href="/home" type="button" class="btn btn-primary">Back</a>
-            <button type="submit" class="btn btn-primary ms-3" form="leave-form" id="submit">Submit</button>
         </div>
     </div>
 

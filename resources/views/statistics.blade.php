b75b68138ffee05ec709da174386e2f73b7319a9
Georgiana Stoie <137824525+Georgiana1254@users.noreply.github.com>
2024-07-24 10:53:09 +0300

    statistic frontend

---
+++ resources/views/statistics.blade.php
@@ -0,0 +1,55 @@
+@extends('Components.layout')
+
+@section('content')
+
+<div class="mt-4">
+    <h3 class="text-center mb-4">Statistics</h3>
+</div>
+<style>
+    .card{
+        border-radius: 8px;
+        box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+        height: 100%;
+    }
+ </style>
+
+<div class="container" style="height: 100%;">
+    <div class="row justify-content-center" style="height: 70%;">
+        <div class="col-6">
+            <div class="card">
+                <div>
+                    
+            
+                </div>
+            </div>
+        </div>
+
+        <div class="col-6">
+            <div class="card ">
+                <div>
+            
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center" style="height: 70%;">
+        <div class="col-6 mt-4">
+            <div class="card">
+                <div>
+            
+                </div>
+            </div>
+        </div>
+
+        <div class="col-6 mt-4">
+            <div class="card">
+                <div>
+            
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection

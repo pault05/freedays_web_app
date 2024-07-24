1217fcd73524b285dec7d0b6d637f23f45ec5cd7
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-07-24 10:40:00 +0300

    user profile added in profile dropdown

--- resources/views/Components/nav-bar.blade.php
+++ resources/views/Components/nav-bar.blade.php
@@ -100,7 +100,7 @@
         </a>
         <ul class="dropdown-menu text-small">
             <li><a class="dropdown-item" href="#">Settings</a></li>
-            <li><a class="dropdown-item" href="#">Profile</a></li>
+            <li><a class="dropdown-item" href="/user-profile">Profile</a></li>
             <li><hr class="dropdown-divider"></li>
             <li><a class="dropdown-item" href="#">Sign out</a></li>
         </ul>

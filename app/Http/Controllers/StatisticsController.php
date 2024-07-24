8166e9c44f28d7b5d51c0e77f7061314a4864d3f
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2024-07-24 14:24:42 +0300

    backend for free days per year

--- app/Http/Controllers/StatisticsController.php
+++ app/Http/Controllers/StatisticsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\FreeDaysRequest;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Carbon\Carbon;
 
 class StatisticsController extends Controller{
 
@@ -13,14 +14,59 @@ class StatisticsController extends Controller{
         $freeDaysRequests = FreeDaysRequest::where('user_id', $userId)->get();
         //dd($freeDaysRequests);
         //$users = User::all();
+        $allFreeDays = FreeDaysRequest::get();
+
+        $daysInYear = FreeDaysRequest::whereYear('starting_date', '=', Carbon::now()->year)->get();
+
+        $freeDaysArray = array();
+        $days = 0;
+        foreach ($allFreeDays as $request) {
+            //dd($request);
+            $year = Carbon::parse($request['starting_date'])->year;
+            //$days += $freeDaysRequests['days'];
+            $days += $request['days'];
+        }
+        //$freeDaysArray['year'] = $year;
+        //$freeDaysArray['days'] = $days;
+        $freeDaysArray = array (
+            array("2023",22),
+            array("2024",15),
+            array("2025",5),
+            array("2026",17)
+        );
+        $data = $freeDaysRequests->map(function ($request){
+            $start = Carbon::parse($request->starting_date);
+            $end = Carbon::parse($request->ending_date);
+            $daysOff = $end->diffInDays($start) + 1;
+
+            return[
+                'month' => $start->format('F Y'),
+                'days' => $daysOff,
+            ];
+        });
+
+        $statistics = $data->groupBy('month')->map(function ($items){
+            return $items->sum('days');
+        });
 
         $requestsByDescription = $freeDaysRequests->groupBy('description')->map(function($item,$key){
             return $item->count();
         });
 
+        $charData = $requestsByDescription->map(function ($count, $description){
+            return [$description, $count];
+        })->values()->toArray();
+
+        return view('statistics',[
+            'statistics' => $statistics,
+            'requestsByDescription' => $requestsByDescription,
+            'charData' => $charData,
+            'daysPerYear' => $freeDaysArray
+        ]);
+
         //dd($requestsByDescription);
 
-        return view('statistics',compact('requestsByDescription'));
+        //return view('statistics',compact('requestsByDescription'));
     }
 
 }
--- resources/views/statistics.blade.php
+++ resources/views/statistics.blade.php
@@ -23,31 +23,32 @@
         }
     </style>
 
-        <div class="container">
-                <div class="row">
-                
-                    <div class="col-lg-6 col-md-6 mb-4">
-                        <div class="card">
-                            <div id="container2">
-                            
-                            </div>
-                        </div>
-                    </div>
+    <div class="container">
+        <div class="row">
 
-        
-                    <div class="col-lg-6 col-md-6 mb-4">
-                        <div class="card">
-                            <div id="container3">
-                            
-                            </div>
-                        </div>
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container2">
+
                     </div>
                 </div>
+            </div>
+
+
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container3">
+                        <h3>Free days per year</h3>
+
+                    </div>
+                </div>
+            </div>
+        </div>
 
 
 
         <div class="row justify-content-center" style="height: 40%;">
-            <div class="col-6 mt-4">
+            <div class="col-6 mt-2">
                 <div class="card">
                     <div id="container">
 
@@ -55,195 +56,206 @@
                 </div>
             </div>
 
-            <div class="col-6 mt-4">
+            <div class="col-6 mt-2">
                 <div class="card">
-                    <div class="d-flex justify-content-center">
-                    <div class="dropdown">
-                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Dropdown button
-                    </button>
-                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                            <a class="dropdown-item" href="#">Action</a>
-                            <a class="dropdown-item" href="#">Another action</a>
-                            <a class="dropdown-item" href="#">Something else here</a>
-                        </div>
+                    <div class="d-flex justify-content-start">
+                        <div class="dropdown ms-1 mt-2">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-expanded="false">
+                                Order By
+                            </button>
+                            <ul class="dropdown-menu text-center" aria-labelledby="dropdownMenuButton">
+                                <li>
+                                    <a  class="btn btn-dark w-75">
+                                        Paid Leave
+                                    </a>
+                                </li>
 
+                                <li><div class="dropdown-divider"></div></li>
+                                {{-- divider--}}
+                                <li>
+                                    <a class="btn btn-dark w-75">
+                                        Unpaid Leave
+                                    </a>
+                                </li>
+                                <li><div class="dropdown-divider"></div></li>
+                                <li>
+                                    <a class="btn btn-dark w-75">
+                                        Medical Leave
+                                    </a>
+                                </li>
+                                <li><div class="dropdown-divider"></div></li>
+                                <li>
+                                    <a class="btn btn-dark w-75">
+                                        Motivated leave
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            Highcharts.chart('container', {
-                chart: {
-                    type: 'column'
-                },
-                title: {
-                    text: 'Number of leaves per month',
-                    align: 'left'
-                },
-                subtitle: {
-                    text:
-                        'Source: <a target="_blank" ' +
-                        'href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>',
-                    align: 'left'
-                },
-                xAxis: {
-                    categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'Octbober', 'November', 'December'],
-                    crosshair: true,
-                    accessibility: {
-                        description: 'Months'
-                    }
-                },
-                yAxis: {
-                    min: 0,
-                    title: {
-                        text: '1000 metric tons (MT)'
-                    }
-                },
-                tooltip: {
-                    valueSuffix: ' (1000 MT)'
-                },
-                plotOptions: {
-                    column: {
-                        pointPadding: 0.2,
-                        borderWidth: 0
-                    }
-                },
-                series: [
-                    {
-                        name: 'Leaves',
-                        data: [387749, 280000, 129000, 64300, 54000, 34300,387749, 280000, 129000, 64300, 54000, 34300]
-                    }
-                ]
-            });
-        });
-    </script>
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    Highcharts.chart('container', {
+                        chart: {
+                            type: 'column'
+                        },
+                        title: {
+                            text: 'Number of leaves per month',
+                            align: 'left'
+                        },
+                        subtitle: {
+                            text:
+                                'Source: <a target="_blank" ' +
+                                'href="https://www.indexmundi.com/agriculture/?commodity=corn">indexmundi</a>',
+                            align: 'left'
+                        },
+                        xAxis: {
+                            categories: ['January', 'February', 'March', 'April', 'May', 'June', 'July','August', 'September', 'Octbober', 'November', 'December'],
+                            crosshair: true,
+                            accessibility: {
+                                description: 'Months'
+                            }
+                        },
+                        yAxis: {
+                            min: 0,
+                            title: {
+                                text: 'days'
+                            }
+                        },
+                        tooltip: {
+                            valueSuffix: ' (1000 MT)'
+                        },
+                        plotOptions: {
+                            column: {
+                                pointPadding: 0.2,
+                                borderWidth: 0
+                            }
+                        },
+                        series: [
+                            {
+                                name: 'Leaves',
+                                data: [387749, 280000, 129000, 64300, 54000, 34300,387749, 280000, 129000, 64300, 54000, 34300]
+                            }
+                        ]
+                    });
+                });
+            </script>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function () {
-            const requestsByDescription = @json($requestsByDescription);
+            <script>
+                document.addEventListener('DOMContentLoaded', function () {
+                    const requestsByDescription = @json($requestsByDescription);
 
-            console.log(requestsByDescription);
-            const data = Object.keys(requestsByDescription).map(key => ({
-                name: key,
-                y: requestsByDescription[key]
-            }));
+                    console.log(requestsByDescription);
+                    const data = Object.keys(requestsByDescription).map(key => ({
+                        name: key,
+                        y: requestsByDescription[key]
+                    }));
 
-            Highcharts.chart('container2', {
-                chart: {
-                    type: 'pie'
-                },
-                title: {
-                    text: 'Free Days Requests by Description'
-                },
-                tooltip: {
-                    valueSuffix: ' requests'
-                },
-                plotOptions: {
-                    series: {
-                        allowPointSelect: true,
-                        cursor: 'pointer',
-                        dataLabels: {
+                    Highcharts.chart('container2', {
+                        chart: {
+                            type: 'pie'
+                        },
+                        title: {
+                            text: 'Free Days Requests by Description'
+                        },
+                        tooltip: {
+                            valueSuffix: ' requests'
+                        },
+                        plotOptions: {
+                            series: {
+                                allowPointSelect: true,
+                                cursor: 'pointer',
+                                dataLabels: {
+                                    enabled: true,
+                                    format: '{point.name}: {point.percentage:.1f} %'
+                                }
+                            }
+                        },
+                        credits: {
+                            enabled: false
+                        },
+                        series: [{
+                            name: 'Requests',
+                            colorByPoint: true,
+                            data: data
+                        }]
+                    });
+                });
+            </script>
+
+            <script>
+                const chart = new Highcharts.Chart({
+                    chart: {
+                        renderTo: 'container3',
+                        type: 'column',
+                        options3d: {
                             enabled: true,
-                            format: '{point.name}: {point.percentage:.1f} %'
+                            alpha: 15,
+                            beta: 15,
+                            depth: 50,
+                            viewDistance: 25
                         }
-                    }
-                },
-                credits: {
-                    enabled: false
-                },
-                series: [{
-                    name: 'Requests',
-                    colorByPoint: true,
-                    data: data
-                }]
-            });
-        });
-    </script>
+                    },
+                    xAxis: {
+                        type: 'category'
+                    },
+                    yAxis: {
+                        title: {
+                            enabled: false
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '<b>{point.key}</b><br>',
+                        pointFormat: 'Cars sold: {point.y}'
+                    },
+                    title: {
+                        text: 'Free days per year',
+                        align: 'center'
+                    },
 
-<script>
-    const chart = new Highcharts.Chart({
-    chart: {
-    renderTo: 'container3',
-    type: 'column',
-    options3d: {
-    enabled: true,
-    alpha: 15,
-    beta: 15,
-    depth: 50,
-    viewDistance: 25
-    }
-    },
-    xAxis: {
-    type: 'category'
-    },
-    yAxis: {
-    title: {
-    enabled: false
-    }
-    },
-    tooltip: {
-    headerFormat: '<b>{point.key}</b><br>',
-    pointFormat: 'Cars sold: {point.y}'
-    },
-    title: {
-    text: 'Sold passenger cars in Norway by brand, May 2024',
-    align: 'left'
-    },
-    subtitle: {
-    text: 'Source: ' +
-    '<a href="https://ofv.no/registreringsstatistikk"' +
-    'target="_blank">OFV</a>',
-    align: 'left'
-    },
-    legend: {
-    enabled: false
-    },
-    plotOptions: {
-    column: {
-    depth: 25
-    }
-    },
-    series: [{
-    data: [
-    ['Toyota', 1795],
-    ['Volkswagen', 1242],
-    ['Volvo', 1074],
-    ['Tesla', 832],
-    ['Hyundai', 593],
-    ['MG', 509],
-    ['Skoda', 471],
-    ['BMW', 442],
-    ['Ford', 385],
-    ['Nissan', 371]
-    ],
-    colorByPoint: true
-    }]
-    });
+                    legend: {
+                        enabled: false
+                    },
+                    plotOptions: {
+                        column: {
+                            depth: 25
+                        }
+                    },
+                    series: [{
+                        data: [
+                            @foreach($daysPerYear as $array)
 
-    function showValues() {
-    document.getElementById(
-    'alpha-value'
-    ).innerHTML = chart.options.chart.options3d.alpha;
-    document.getElementById(
-    'beta-value'
-    ).innerHTML = chart.options.chart.options3d.beta;
-    document.getElementById(
-    'depth-value'
-    ).innerHTML = chart.options.chart.options3d.depth;
-    }
+                            ['{{ $array[0] }}', {{ $array[1]  }}],
+                            @endforeach
 
-    document.querySelectorAll(
-    '#sliders input'
-    ).forEach(input => input.addEventListener('input', e => {
-    chart.options.chart.options3d[e.target.id] = parseFloat(e.target.value);
-    showValues();
-    chart.redraw(false);
-    }));
+                        ],
+                        colorByPoint: true
+                    }]
+                });
 
-    showValues();
+                function showValues() {
+                    document.getElementById(
+                        'alpha-value'
+                    ).innerHTML = chart.options.chart.options3d.alpha;
+                    document.getElementById(
+                        'beta-value'
+                    ).innerHTML = chart.options.chart.options3d.beta;
+                    document.getElementById(
+                        'depth-value'
+                    ).innerHTML = chart.options.chart.options3d.depth;
+                }
 
-</script>
+                document.querySelectorAll(
+                    '#sliders input'
+                ).forEach(input => input.addEventListener('input', e => {
+                    chart.options.chart.options3d[e.target.id] = parseFloat(e.target.value);
+                    showValues();
+                    chart.redraw(false);
+                }));
+
+                showValues();
+
+            </script>
 @endsection

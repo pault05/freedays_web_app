9361b773d01e8b85c7b43f6019f67849604ce2d2
pault05 <147524535+pault05@users.noreply.github.com>
2024-07-26 13:58:51 +0300

    Soft Deletes
    UserFile, FreeDaysReqFile

--- app/Models/FreeDaysReqFile.php
+++ app/Models/FreeDaysReqFile.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class FreeDaysReqFile extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'free_days_req_id',

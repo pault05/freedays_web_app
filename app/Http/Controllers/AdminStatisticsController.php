fda0259e8c2c038866bbd5208e2e21905e9ee07e
Edina Biro <biro.edina.anita@student.ms.sapientia.ro>
2024-07-29 13:46:13 +0300

    update admin statistics

--- app/Http/Controllers/AdminStatisticsController.php
+++ app/Http/Controllers/AdminStatisticsController.php
@@ -1,9 +1,197 @@
 <?php
 namespace App\Http\Controllers;
+use App\Models\FreeDaysRequest;
 use Illuminate\Http\Request;
+use App\Http\Requests;
+use Carbon\Carbon;
+use App\Models\Category;
 
-class AdminStatisticsController extends Controller{
-    public function index(){
-        return view('admin_statistics');
+class AdminStatisticsController extends Controller
+{
+    public function index()
+    {
+        //$userId = auth()->user()->id;
+
+        $statistics = $this->getStatistics(auth()->user()->id);
+        $requestsByCategory = $this->getUserByStatistics();
+        $daysPerYear = $this->getDaysPerYear();
+        $daysPerMonth = $this->getDaysPerMonth();
+        $formattedData = $this->getFormattedData();
+
+
+        return view('statistics', [
+            'statistics' => $statistics,
+            'requestsByCategory' => $requestsByCategory,
+            'daysPerYear' => $daysPerYear,
+            'daysPerMonth' => $daysPerMonth,
+            'formattedData' => $formattedData
+
+        ]);
+
+
     }
+
+    private function getStatistics($userId)
+    {
+        $freeDayRequets = FreeDaysRequest::where('user_id', $userId)->with('category')->get();
+
+        $data = $freeDayRequets->map(function ($request) {
+            $start = Carbon::parse($request->starting_date);
+            $end = Carbon::parse($request->ending_date);
+            $daysOff = $end->diffInDays($start) + 1;
+
+            return [
+                'month' => $start->format('F Y'),
+                'days' => $daysOff,
+            ];
+        });
+
+        return $data->groupBy('month')->map(function ($items) {
+            return $items->sum('days');
+        });
+    }
+
+    private function getUserByStatistics()
+    {
+        $freeDaysRequests = FreeDaysRequest::with('category')->get();
+
+        return $freeDaysRequests->groupBy('category.name')->map(function ($items) {
+            return $items->count();
+        });
+    }
+
+    private function getDaysPerYear()
+    {
+        $allFreeDays = FreeDaysRequest::with('category')->get();
+
+        return $allFreeDays->groupBy(function ($request) {
+            return Carbon::parse($request['starting_date'])->year;
+        })->map(function ($yearRequests) {
+            return $yearRequests->sum('days');
+        })->toArray();
+    }
+//    private function getDaysPerYear()
+//    {
+//        $allFreeDays = FreeDaysRequest::with('category')->get();
+//
+//        $daysPerYear = $allFreeDays->flatMap(function ($request) {
+//            $start = Carbon::parse($request->starting_date);
+//            $end = Carbon::parse($request->ending_date);
+//            $daysOff = $end->diffInDays($start) + 1;
+//
+//            $years = [];
+//            for ($year = $start->year; $year <= $end->year; $year++) {
+//                $startOfYear = Carbon::create($year, 1, 1);
+//                $endOfYear = Carbon::create($year, 12, 31);
+//
+//                $startOfPeriod = $start->greaterThan($startOfYear) ? $start : $startOfYear;
+//                $endOfPeriod = $end->lessThan($endOfYear) ? $end : $endOfYear;
+//                $daysInYear = $endOfPeriod->diffInDays($startOfPeriod) + 1;
+//
+//                // Aggregate days in the year
+//                if (isset($years[$year])) {
+//                    $years[$year] += $daysInYear;
+//                } else {
+//                    $years[$year] = $daysInYear;
+//                }
+//            }
+//
+//            return $years;
+//        })->reduce(function ($carry, $item) {
+//            foreach ($item as $year => $days) {
+//                if (isset($carry[$year])) {
+//                    $carry[$year] += $days;
+//                } else {
+//                    $carry[$year] = $days;
+//                }
+//            }
+//            return $carry;
+//        }, []);
+//
+//        return $daysPerYear;
+//    }
+
+
+//    private function getDaysPerMonth($userId){
+//        $freeDaysRequests = FreeDaysRequest::with('category')->get();
+//
+//        $daysPerMonth = $freeDaysRequests->map(function($request){
+//            $start = Carbon::parse($request->starting_date);
+//            $end = Carbon::parse($request->ending_date);
+//            $daysOff = $end->diffInDays($start)+1;
+//
+//
+//            return [
+//                'month'=> $start->format('F'),
+//                'days' => $daysOff,
+//            ];
+//        })->groupBy('month')->map(function($items){
+//            return $items->sum('days');
+//        });
+//
+//        $months = collect(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+//        return $months->mapWithKeys(function ($month) use ($daysPerMonth){
+//            return [$month => $daysPerMonth->get($month,0)];
+//        });
+//    }
+    private function getDaysPerMonth()
+    {
+        $freeDaysRequests = FreeDaysRequest::all();
+        $daysPerMonth = collect();
+
+        foreach ($freeDaysRequests as $request) {
+            $start = Carbon::parse($request->starting_date);
+            $end = Carbon::parse($request->ending_date);
+
+            if ($end->lessThan($start)) {
+                continue;
+            }
+
+            while ($start->lte($end)) {
+                $monthEnd = $start->copy()->endOfMonth();
+                $daysInMonth = min($end->copy()->endOfMonth()->diffInDays($start) + 1, $monthEnd->diffInDays($start) + 1);
+
+                $month = $start->format('F Y');
+                $daysPerMonth->put($month, ($daysPerMonth->get($month, 0) + $daysInMonth));
+                $start->addMonth()->startOfMonth();
+            }
+        }
+
+        $months = collect(['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']);
+
+        $currentYear = Carbon::now()->year;
+
+        return $months->mapWithKeys(function ($month) use ($daysPerMonth, $currentYear) {
+            $formattedMonth = "{$month} {$currentYear}";
+            return [$month => $daysPerMonth->get($formattedMonth, 0)];
+        });
+    }
+
+
+    private function getFormattedData()
+    {
+        $categories = Category::all();
+        $users = \App\Models\User::all();
+        $chartData = [];
+
+        foreach ($users as $user) {
+            $userData = ['user' => $user->name];
+
+            foreach ($categories as $category) {
+                $count = FreeDaysRequest::where('user_id', $user->id)
+                        ->where('category_id', $category->id)
+                        ->count();
+
+                    $userData[$category->name] = $count;
+                }
+                $chartData[] = $userData;
+            }
+
+            return [
+                'categories' => $categories->pluck('name'),
+                'data' => $chartData
+            ];
+        }
+
 }
+
--- resources/views/admin_statistics.blade.php
+++ resources/views/admin_statistics.blade.php
@@ -2,4 +2,283 @@
 
 @section('content')
 
+    <script src="https://code.highcharts.com/highcharts.js"></script>
+    <script src="https://code.highcharts.com/modules/exporting.js"></script>
+    <script src="https://code.highcharts.com/modules/accessibility.js"></script>
+    <script src="https://code.highcharts.com/highcharts-3d.js"></script>
+    <script src="https://code.highcharts.com/modules/export-data.js"></script>
+
+    <!--<script src="https://code.highcharts.com/themes/dark-unica.js"></script> -->
+
+    <div class="container-main d-flex flex-column justify-content-center align-items-center">
+        <div class="card p-3 shadow-sm mb-5 w-50 mt-3 bg-primary">
+            <h1 class="text-center w-auto" style="text-shadow: 2px 2px 4px black;color: white">Statistics</h1>
+        </div>
+
+
+    <style>
+        /*.card {*/
+        /*    border-radius: 8px;*/
+        /*    box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);*/
+        /*    height: 100%;*/
+        /*    width: 100%;*/
+        /*}*/
+    </style>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container2">
+
+                    </div>
+                </div>
+            </div>
+
+
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container3">
+                        <h3>Free days per year</h3>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container">
+
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-6 col-md-6 mb-4">
+                <div class="card">
+                    <div id="container4">
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const daysPerMonth = @json($daysPerMonth);
+
+                Highcharts.chart('container', {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: 'Number of leaves per month',
+                        align: 'center'
+                    },
+                    xAxis: {
+                        categories: Object.keys(daysPerMonth),
+                        crosshair: true,
+                        accessibility: {
+                            description: 'Months'
+                        }
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'days'
+                        }
+                    },
+                    plotOptions: {
+                        column: {
+                            pointPadding: 0.2,
+                            borderWidth: 0
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    series: [
+                        {
+                            name: 'Leaves',
+                            <!-- data: @json(array_values($daysPerMonth->toArray())),-->
+                            {{--data: @json($daysPerMonth)--}}
+                            data : Object.values(daysPerMonth)
+
+                        }
+                    ]
+                });
+            });
+        </script>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const requestsByCategory = @json($requestsByCategory);
+                const data = Object.keys(requestsByCategory).map(key => ({
+                    name: key,
+                    y: requestsByCategory[key]
+                }));
+
+                Highcharts.chart('container2', {
+                    chart: {
+                        type: 'pie'
+                    },
+                    title: {
+                        text: 'Free Days Requests By Categories'
+                    },
+                    tooltip: {
+                        valueSuffix: ' requests'
+                    },
+                    plotOptions: {
+                        series: {
+                            allowPointSelect: true,
+                            cursor: 'pointer',
+                            dataLabels: {
+                                enabled: true,
+                                format: '{point.name}: {point.percentage:.1f} %'
+                            }
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    series: [{
+                        name: 'Requests',
+                        colorByPoint: true,
+                        data: data
+                    }]
+                });
+            });
+        </script>
+
+        {{--        <!-- debug -->--}}
+        {{--        <pre>--}}
+        {{--    {{ print_r($daysPerYear, true) }}--}}
+        {{--</pre>--}}
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const daysPerYear = @json($daysPerYear);
+                const data = Object.entries(daysPerYear).map(([year, days]) => [year, days]);
+
+                Highcharts.chart('container3', {
+                    chart: {
+                        type: 'column',
+                        options3d: {
+                            enabled: true,
+                            alpha: 15,
+                            beta: 15,
+                            depth: 50,
+                            viewDistance: 25
+                        }
+                    },
+                    xAxis: {
+                        //type: 'category'
+                        categories: data.map(([year])=>year),
+                        title: {
+                            text: 'Year'
+                        }
+                    },
+                    yAxis: {
+                        title: {
+                            text: 'Days off'
+                        }
+                    },
+                    tooltip: {
+                        headerFormat: '<b>{point.key}</b><br>',
+                        pointFormat: 'Days off: {point.y}'
+                    },
+                    title: {
+                        text: 'Number of leaves per year',
+                        align: 'center'
+                    },
+                    subtitle: {
+                        text: '3D chart'
+                    },
+                    legend: {
+                        enabled: false
+                    },
+                    plotOptions: {
+                        column: {
+                            depth: 25
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    series: [{
+                        data: data.map(([year,days]) => days),
+                        name: 'Total days off'
+                    }]
+                });
+            });
+        </script>
+
+        <script>
+            document.addEventListener('DOMContentLoaded', function () {
+                const formattedData = @json($formattedData);
+
+                const seriesData = formattedData.categories.map((category) => ({
+                    name: category,
+                    data: formattedData.data.map(item => item[category] || 0)
+                }));
+
+
+
+                Highcharts.chart('container4', {
+                    chart: {
+                        type: 'column'
+                    },
+                    title: {
+                        text: 'Leaves per Category per Year',
+                        align: 'center'
+                    },
+                    xAxis: {
+                        categories: formattedData.years
+                    },
+                    yAxis: {
+                        min: 0,
+                        title: {
+                            text: 'Number of Leaves'
+                        },
+                        // stackLabels: {
+                        //     enabled: true
+                        // }
+                    },
+                    legend: {
+                        align: 'left',
+                        x: 70,
+                        verticalAlign: 'top',
+                        y: 70,
+                        floating: true,
+                        backgroundColor:
+                            Highcharts.defaultOptions.legend.backgroundColor || 'white',
+                        borderColor: '#CCC',
+                        borderWidth: 1,
+                        shadow: false
+                    },
+                    tooltip: {
+                        headerFormat: '<b>{point.x}</b><br/>',
+                        pointFormat: '{series.name}: {point.y}<br/>Total: {point.stackTotal}'
+                    },
+                    plotOptions: {
+                        column: {
+                            stacking: 'normal',
+                            dataLabels: {
+                                enabled: true
+                            }
+                        }
+                    },
+                    credits: {
+                        enabled: false
+                    },
+                    series: seriesData
+                });
+            });
+        </script>
+
+
 @endsection

c7746ca289c70619f7e86aeee2a3e43544df5156
Georgiana Stoie <137824525+Georgiana1254@users.noreply.github.com>
2024-07-30 14:07:14 +0300

    request frontend

--- resources/views/Components/theme_button.blade.php
+++ resources/views/Components/theme_button.blade.php
@@ -1,10 +1,10 @@
 <script src="https://getbootstrap.com/docs/5.3/assets/js/color-modes.js"></script>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/">
+    <!-- <link rel="canonical" href="https://getbootstrap.com/docs/5.3/examples/dashboard/"> -->
 
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@docsearch/css@3">
 
-    <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- <link href="https://getbootstrap.com/docs/5.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> -->
 
     <!-- Favicons -->
     <link rel="apple-touch-icon" href="https://getbootstrap.com/docs/5.3/assets/img/favicons/apple-touch-icon.png" sizes="180x180">
@@ -99,7 +99,7 @@
     <!-- Custom styles for this template -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.min.css" rel="stylesheet">
     <!-- Custom styles for this template -->
-    <link href="https://getbootstrap.com/docs/5.3/examples/dashboard/dashboard.css" rel="stylesheet">
+    <!-- <link href="https://getbootstrap.com/docs/5.3/examples/dashboard/dashboard.css" rel="stylesheet"> -->
 
 <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
     <symbol id="check2" viewBox="0 0 16 16">
--- resources/views/free_day_request.blade.php
+++ resources/views/free_day_request.blade.php
@@ -20,12 +20,12 @@
 
     <br>
 
-    <div class="card p-5 shadow mb-5 w-100 col-sm-1 col-md-3">
+    <div class="card p-5 shadow mb-5 w-100">
 
         <div class="container mt-3">
             <form id="leave-form" action="/free-day-request/save" method="POST"  enctype="multipart/form-data">
-                <div class="row">
-                    <div class="col-12 col-sm-6 col-md-4 mb-3">
+                <div class="row d-flex justify-content-start">
+                    <div class="col-12 col-sm-6 col-md-4 mb-3 ">
                         <label for="days-left">Days off left</label>
                         <p class="ms-1">{{ 21 - $request_leave['approved'] }}</p>
                     </div>
@@ -47,44 +47,39 @@
                         <input type="hidden" class="form-control" name="days" id="days"
                                value="{{ session('days_left', '0') }}" placeholder="Selected leave days">
                     </div>
-                    <div id="half-day-container" class="col-sm-12 col-md-6 col-lg-3 form-check mt-1 mb-2" style="display: none">
+                    <div id="half-day-container" class="col-sm-12 col-md-6 col-lg-3 form-check mt-3 mb-2" style="display: none">
                         <br>
                         <input type="checkbox" class="form-check-input" name="half-day" id="half-day">
                         <input type="hidden" id="half-day-state" name="half-day-state" value="0">
                         <label class="form-check-label" for="half-day">Half day</label>
                     </div>
-                    </div>
-                </div>
+               </div>
 
                 <div id="error-message" class="alert alert-danger" style="display: none;"></div>
 
 
-                <div class="row">
-                    <div class="mb-3 col mb-2">
-                        <label for="category" class="col-sm-2 col-form-label">Category</label>
-                            <div class="col-sm-10">
-                                <select class="form-select" name="category_id" style="width: 110%">
+                <div class="row d-flex justify-content-start">
+                        <label for="category" class="col-sm-12 col-form-label ">Category</label>
+                            <div class="col-sm-12">
+                                <select class="form-select" name="category_id" style="width: 92%">
                                     @foreach($categories as $category)
                                         <option value="{{ $category->id }}">{{ $category->name }}</option>
                                     @endforeach
                                 </select>
                             </div>
-                    </div>
                 </div>
 
-                <div class="row">
-                    <div class="mb-2">
+                <div class="row d-flex justify-content-start mb-2 mt-3">
+                    <div class="col">
                         <label for="description" class="form-label">Description</label>
                         <textarea class="form-control" name="description" rows="3" placeholder="Description" style="width:92%"></textarea>
                     </div>
                 </div>
 
                 <div class="row mt-3">
-                    <div class="mt-3 row">
-                        <div class="col-sm-6" style="width: 94.5%">
+                     <div class="col-sm-6">
                             <input type="file" class="form-control mb-1" style="width: 100%" name="proof">
-                        </div>
-                    </div>
+                     </div>
                 </div>
                 <style>
                     .btnOutline {
@@ -102,15 +97,14 @@
                 </style>
 
                 <div class="row ms-5 mt-5">
-                    <div class="d-flex justify-content-end" style="margin-left: 89%; width: 3%">
-                        <a href="/home" type="button" class="btn btn-primary" style="margin-right:13%">Back</a>
-                        <button type="submit" class="btn btn-primary" form="leave-form" id="submit">Submit</button>
+                    <div class="d-flex justify-content-end" style="margin-left: 89%; width: 2%">
+                        <a href="/home" type="button" class="btn btn-dark" style="margin-right:13%">Back</a>
+                        <button type="submit" class="btn btn-dark" form="leave-form" id="submit">Submit</button>
                     </div>
                 </div>
             </form>
       </div>
     </div>
-                </div>
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.29.4/moment.min.js"></script>
@@ -145,7 +139,7 @@
                 return holiday.isSame(currentDate, 'day'); // compara doar ziua
             });
 
-            // verifica dacă este zi de lucru (nu duminică, sâmbătă sau sărbătoare)
+            // verifica dacă este zi de lucru
             if (currentDate.day() !== 0 && currentDate.day() !== 6 && !isHoliday) {
                 totalDays++;
             }

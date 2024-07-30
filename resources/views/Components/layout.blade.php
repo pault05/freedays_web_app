034e82e8bc6c9d3dcf370903d10a0b6b9fa8ff89
SerbanPrecup <precup.serban@yahoo.com>
2024-07-30 16:08:27 +0300

    Layout update for Admin View Users

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -31,6 +31,8 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 <script src="https://getbootstrap.com/docs/5.2/examples/sidebars/sidebars.js"></script>
 
+
+@stack('scripts')
 </body>
 </html>
 

54522a1a6160fbd0fe99c43f901ee056f88053d0
Voda Tudor-Andrei <tvoda25@gmail.com>
2024-07-31 14:03:00 +0300

    login revamped nav bar revamped deleted sidebard

--- resources/views/Components/layout.blade.php
+++ resources/views/Components/layout.blade.php
@@ -6,22 +6,17 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>{{ env('APP_NAME')}}</title>
-    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">
+    {{--    <link href="{{ asset('css/bootstrap.css') }}" rel="stylesheet">--}}
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.1.2/css/dataTables.dataTables.min.css">
 </head>
 <body>
-<header class="navbar sticky-top bg-dark flex-md-nowrap p-0 shadow" data-bs-theme="dark">
-    <x-nav-bar></x-nav-bar>
-</header>
-    <x-theme_button></x-theme_button>
-        <div class="container-fluid">
-            <div class="row">
-                <x-sidebar></x-sidebar>
-                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-                    @yield('content')
-                </main>
-            </div>
-        </div>
+<x-nav-bar></x-nav-bar>
+<div class="container-fluid">
+    <main class="">
+        @yield('content')
+    </main>
+</div>
 
 
 
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,115 +1,52 @@
 @extends('Components.login-layout')
 
 @section('content')
-    <style>
-        .bd-placeholder-img {
-            font-size: 1.125rem;
-            text-anchor: middle;
-            -webkit-user-select: none;
-            -moz-user-select: none;
-            user-select: none;
-        }
-
-        @media (min-width: 768px) {
-            .bd-placeholder-img-lg {
-                font-size: 3.5rem;
-            }
-        }
-
-        .b-example-divider {
-            width: 100%;
-            height: 3rem;
-            background-color: rgba(0, 0, 0, .1);
-            border: solid rgba(0, 0, 0, .15);
-            border-width: 1px 0;
-            box-shadow: inset 0 .5em 1.5em rgba(0, 0, 0, .1), inset 0 .125em .5em rgba(0, 0, 0, .15);
-        }
-
-        .b-example-vr {
-            flex-shrink: 0;
-            width: 1.5rem;
-            height: 100vh;
-        }
-
-        .bi {
-            vertical-align: -.125em;
-            fill: currentColor;
-        }
-
-        .nav-scroller {
-            position: relative;
-            z-index: 2;
-            height: 2.75rem;
-            overflow-y: hidden;
-        }
-
-        .nav-scroller .nav {
-            display: flex;
-            flex-wrap: nowrap;
-            padding-bottom: 1rem;
-            margin-top: -1px;
-            overflow-x: auto;
-            text-align: center;
-            white-space: nowrap;
-            -webkit-overflow-scrolling: touch;
-        }
-
-        .btn-bd-primary {
-            --bd-violet-bg: #712cf9;
-            --bd-violet-rgb: 112.520718, 44.062154, 249.437846;
-
-            --bs-btn-font-weight: 600;
-            --bs-btn-color: var(--bs-white);
-            --bs-btn-bg: var(--bd-violet-bg);
-            --bs-btn-border-color: var(--bd-violet-bg);
-            --bs-btn-hover-color: var(--bs-white);
-            --bs-btn-hover-bg: #6528e0;
-            --bs-btn-hover-border-color: #6528e0;
-            --bs-btn-focus-shadow-rgb: var(--bd-violet-rgb);
-            --bs-btn-active-color: var(--bs-btn-hover-color);
-            --bs-btn-active-bg: #5a23c8;
-            --bs-btn-active-border-color: #5a23c8;
-        }
-
-        .bd-mode-toggle {
-            z-index: 1500;
-        }
-
-        .bd-mode-toggle .dropdown-menu .active .bi {
-            display: block !important;
-        }
-    </style>
-
-
+    <link media="all" type="text/css" rel="stylesheet" href="https://cloud.spinoffice-crm.com/ubold_light/css/common_style.css?ver=20240731.1">
+    <link media="all" type="text/css" rel="stylesheet" href="https://cloud.spinoffice-crm.com/ubold_light/css/ubold.css?ver=20240731.1">
     <link href="../../public/css/login.css" rel="stylesheet">
     <body class="d-flex align-items-center py-4 bg-body-tertiary">
 
+    <div id="wrapper">
+        <div class="account-pages mt-5 mb-5">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-md-8 col-lg-6 col-xl-5">
+                        <div class="card bg-pattern">
+                            <div class="card-body p-4">
+                                <div class="text-center w-75 m-auto">
+                                    <a href='/login'>
+                                        <h4 class="text-secondary">
+                                            <img class="mb-4 img-fluid" src={{asset("/images/login_logo.png")}} alt="" width="293" height="52">
+                                        </h4>
+                                    </a>
+                                    <p class="text-muted mb-4 mt-3">Log in to your account</p>
 
-    <h1 style="padding-top: 80px; text-align: center; padding-bottom: 20px"  >Welcome to Vacation Vault!</h1>
-    <main class="form-signin m-auto" style="width: 45%">
-        <form method="POST" action="/login" style="text-align: center">
-            @csrf
-            <img class="mb-4 img-fluid" src={{asset("/images/login_logo.png")}} alt="" width="293" height="52">
-            <h1 class="h3 mb-3 fw-normal" style="padding: 10px">Please Log In</h1>
+                                </div>
 
-            <div class="form-floating" style="padding: 2px">
-                <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email"
-                       value="{{old('email')}}" required>
-                <label for="floatingInput">Email address</label>
+                                <form method="POST" action="/login">
+                                    @csrf
+                                    <div class="form-group mb-3" style="padding: 2px">
+                                        <label for="email">Email address</label>
+                                        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" name="email"
+                                               value="{{old('email')}}" required>
+                                    </div>
+                                    <x-form-error name="email"/>
+                                    <div class="form-group mb-3" style="padding: 2px">
+                                        <label for="password">Password</label>
+                                        <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" required>
+                                    </div>
+                                    <button class="btn btn-primary btn-block" type="submit">Log In</button>
+                                    <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
+                                </form>
+                            </div> <!-- end card-body -->
+                        </div>
+                        <!-- end card -->
+                    </div> <!-- end col -->
+                </div>
+                <!-- end row -->
             </div>
-            <x-form-error name="email"/>
-            <div class="form-floating" style="padding: 2px">
-                <input type="password" class="form-control" id="floatingPassword" placeholder="Password" name="password" required>
-                <label for="floatingPassword">Password</label>
-            </div>
-            <div class="form-check text-start my-3">
-                <input class="form-check-input" type="checkbox" value="remember-me" id="flexCheckDefault">
-                <label class="form-check-label" for="flexCheckDefault">
-                    Remember me
-                </label>
-            </div>
-            <button class="btn btn-primary w-100 py-2" type="submit">Log In</button>
-            <p class="mt-5 mb-3 text-body-secondary">&copy; 2017–2024</p>
-        </form>
-    </main>
+            <!-- end container -->
+        </div>
+    </div>
+
 @endsection
